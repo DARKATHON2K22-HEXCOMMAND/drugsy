f07f90062108eab1b28044e332adf1de3faf8acb
Karsh Trivedi <karsh.h.trivedi@gmail.com>
2022-08-29 16:29:25 +0530

    Update test.py
    read txt file via pandas

--- test.py
+++ test.py
@@ -1,9 +1,10 @@
 import drugsy
+import pandas as pd
 
 ds = drugsy.Drugsy()
 
-drugs = ["Acamprosate","Actiq","Adderall","Alprazolam","Ambien","Amobarbital","Amphetamine","Amytal","Anexsia","Antabuse","Ativan","Avinza","Buprenorphine","Butalbital","Butorphanol","Campral","Carisoprodol","Chlordiazepoxide","Clonazepam","Clonidine","Cocaine","Codeine","Concerta","Crystal","Darvocet","Darvon","Demerol","Depade","Desoxyn","Dexedrine","Dextroamphetamine","Dextromethorphan","Di-Gesic","Diazepam","Dilaudid","Disulfiram","Duragesic","Duramorph","Ecstasy","Fentanyl","Fioricet","Fiorinal","Flunitrazepam","GHB","Halcion","Hash","Heroin","Hycodan","Hydrocodone","Hydromorphone","Kadian","Ketamine","Klonopin","LSD","Librium","Lorazepam","Lortab","Luminal","MSIR","Marijuana","Meperidine","Mescaline","Methadone","Methamphetamine","Methaqualone","Methylphenidate","Morphine","Mushrooms","Naltrexone","Nembutal","Norco","OxyContin","Oxycodone","PCP","Palladone","Pentobarbital","Percocet","Percodan","Peyote","Phenobarbital","Quaalude","Ritalin","Rohypnol","Roxicodone","Ryzolt","Secobarbital","Seconal","Soma","Speed","Steroids","Sublimaze","Suboxone","Subutex","Tramadol","Triazolam","Tussionex","Ultram","Valium","Vicodin","Vivitrol","Xanax","Xodol","Zolpidem"]
-
+#drugs = ["Acamprosate","Actiq","Adderall","Alprazolam","Ambien","Amobarbital","Amphetamine","Amytal","Anexsia","Antabuse","Ativan","Avinza","Buprenorphine","Butalbital","Butorphanol","Campral","Carisoprodol","Chlordiazepoxide","Clonazepam","Clonidine","Cocaine","Codeine","Concerta","Crystal","Darvocet","Darvon","Demerol","Depade","Desoxyn","Dexedrine","Dextroamphetamine","Dextromethorphan","Di-Gesic","Diazepam","Dilaudid","Disulfiram","Duragesic","Duramorph","Ecstasy","Fentanyl","Fioricet","Fiorinal","Flunitrazepam","GHB","Halcion","Hash","Heroin","Hycodan","Hydrocodone","Hydromorphone","Kadian","Ketamine","Klonopin","LSD","Librium","Lorazepam","Lortab","Luminal","MSIR","Marijuana","Meperidine","Mescaline","Methadone","Methamphetamine","Methaqualone","Methylphenidate","Morphine","Mushrooms","Naltrexone","Nembutal","Norco","OxyContin","Oxycodone","PCP","Palladone","Pentobarbital","Percocet","Percodan","Peyote","Phenobarbital","Quaalude","Ritalin","Rohypnol","Roxicodone","Ryzolt","Secobarbital","Seconal","Soma","Speed","Steroids","Sublimaze","Suboxone","Subutex","Tramadol","Triazolam","Tussionex","Ultram","Valium","Vicodin","Vivitrol","Xanax","Xodol","Zolpidem"]
+drugs = read_csv("drugs.txt", sep " ", header=none)
 # for d in range(len(drugs)):
 #     ds.scrape_torch(drugs[d])
 #     ds.scrape_devil_search(drugs[d])
